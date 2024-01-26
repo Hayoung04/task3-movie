bbe652307284c027729336b4866e550d2e11be15
Hayoung04 <22300209@handong.ac.kr>
2024-01-27 04:07:50 +0900

    css 추가

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,23 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 function Movie({ id, CoverImg, title, summary, genres }) {
   return (
-    <div>
+    <div className={styles.movie}>
       <img src={CoverImg} alt={title} />
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+      <p>
+        <p>
+          <b>Summary</b>
+        </p>{" "}
+        {summary.length > 100 ? `${summary.slice(0, 100)}...` : summary}
+      </p>
+      <br></br>
+      <p>
+        <b>Genres</b>
+      </p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,42 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
   const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log(json);
+    try {
+      const response = await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      );
+      const data = await response.json();
+      setMovie(data.data.movie);
+    } catch (error) {
+      console.error("영화 정보를 가져오는 중 오류 발생:", error);
+    }
   };
+
   useEffect(() => {
     getMovie();
-  }, []);
-  return <h1>Detail</h1>;
+  }, [id]);
+
+  return (
+    <div>
+      <h1>-Detail-</h1>
+      {movie ? (
+        <div>
+          <h2>{movie.title}</h2>
+          <p>Year: {movie.year}</p>
+          <p>Rating: {movie.rating}</p>
+          <p>Genres: {movie.genres.join(", ")}</p>
+          <p>Synopsis: {movie.description_full}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
+
 export default Detail;
