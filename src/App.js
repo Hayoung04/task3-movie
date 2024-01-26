88a5577b9cdf0c43a7871ed3822cd91ec29f84af
Hayoung04 <22300209@handong.ac.kr>
2024-01-27 01:59:34 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
+        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
         {/* <Route path="/" element={<Test />} /> */}
       </Routes>
     </Router>
